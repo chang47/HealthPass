71e63f6f13297a2fcfada722ae02eebc262574c9
chang47 <coralbue@gmail.com>
2016-01-19 19:40:32 -0800

    added values

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,10 +7,20 @@ angular.module('app.controllers', [])
 .controller('page1Ctrl', function($scope) {
 	var currentYear = new Date().getFullYear()
 	var years = [];
+	var days = [];
+	var months = []
 	for (var i = currentYear - 100; i <= currentYear; i++) {
 		years.push(i);
 	}
+	for (var i = 1; i <= 31; i++) {
+		days.push(i);
+	}
+	for (var i = 1; i <= 12; i++) {
+		months.push(i);
+	}
 	$scope.years = years;
+	$scope.months = months;
+	$scope.days = days;
 })
    
 .controller('page2Ctrl', function($scope) {
